8cf8f43f9914e8ff7f6410d1fb9f977e009a8715
JoaoPedro-SA <antunesjoaopedro3@gmail.com>
2025-11-15 15:34:31 -0300

    ajustando rotas do user

--- BackEnd/controller/usuario.py
+++ BackEnd/controller/usuario.py
@@ -21,17 +21,18 @@ def post_user():
         response = usuario.insereUsuario(data.get('nome'), data.get('email'))
         return response
 
-@url.route("/user", methods=["PUT"], strict_slashes=False)
-def update_user():
-    nome = flask.request.args.get("nome")
-    email = flask.request.args.get("email")
-    user_id = flask.request.args.get("user_id")
+@url.route("/user/<int:user_id>", methods=["PUT"], strict_slashes=False)
+def update_user(user_id):
+    data = flask.request.get_json()
+
+    nome = data.get("nome")
+    email = data.get("email")
+
     response = usuario.atualizarUsuario(user_id, nome, email)
     return response
+
         
-@url.route("/user",methods=["DELETE"], strict_slashes=False)
-def delete_user():
-        user_id = flask.request.args.get("user_id")
-        response = usuario.deletarUsuario(user_id)
-        return response
-        
+@url.route("/user/<int:user_id>", methods=["DELETE"], strict_slashes=False)
+def delete_user(user_id):
+    response = usuario.deletarUsuario(user_id)
+    return response
